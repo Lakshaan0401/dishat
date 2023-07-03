a2cf51e0397a44968d52551df656620f18358b87
Lakshaan0401 <99709495+Lakshaan0401@users.noreply.github.com>
2023-07-03 19:29:53 +0530

    Update dishat.js

--- dishat.js
+++ dishat.js
@@ -3,6 +3,16 @@ function login(){
     password = document.getElementById("password").value;
     localStorage.setItem("user_name",user_name);
     localStorage.setItem("password",password);
+    let showPasswordBtn = document.querySelector('.show-password');
+let passwordInp = document.querySelector('.password-input');
+
+showPasswordBtn.addEventListener('click', () => {
+
+    showPasswordBtn.classList.toggle('fa-eye'); 
+    showPasswordBtn.classList.toggle('fa-eye-slash');
+    passwordInp.type = passwordInp.type === 'password' ? 'text' : 'password';
+
+})
     window.location = "dishat_room.html";
 }
 

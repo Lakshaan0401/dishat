96f32eb232c42d8272123a87e42cc8a6df2c6a5e
Lakshaan0401 <99709495+Lakshaan0401@users.noreply.github.com>
2023-07-03 19:30:28 +0530

    Update signup.js

--- signup.js
+++ signup.js
@@ -5,9 +5,44 @@ function signup(){
     localStorage.setItem("email",email);
     localStorage.setItem("user_name",user_name);
     localStorage.setItem("password",password);
+    let showPasswordBtn = document.querySelector('.show-password');
+let passwordInp = document.querySelector('.password-input');
+
+showPasswordBtn.addEventListener('click', () => {
+
+    showPasswordBtn.classList.toggle('fa-eye'); 
+    showPasswordBtn.classList.toggle('fa-eye-slash');
+    passwordInp.type = passwordInp.type === 'password' ? 'text' : 'password';
+
+})
+
+let passwordChecklist = document.querySelectorAll('.list-item');
+
+let validationRegex = [
+    { regex: /.{8,}/ },
+    { regex: /[0-9]/ },
+    { regex: /[a-z]/ },
+    { regex: /[A-Z]/},
+    { regex: /[^A-Za-z0-9]/}
+]
+
+passwordInp.addEventListener('keyup', () => {
+    validationRegex.forEach((item, i) => {
+
+        let isValid = item.regex.test(passwordInp.value);
+
+        if(isValid) {
+            passwordChecklist[i].classList.add('checked');
+        } else{
+            passwordChecklist[i].classList.remove('checked');
+        }
+
+    })
+})
     window.location = "dishat_room.html";
 }
 
 function login(){
     window.location = "index.html";
 }
+

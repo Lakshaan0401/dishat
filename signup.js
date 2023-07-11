7df89ad4e9802b107ba7ffadf08e92ee54ab5ca9
Lakshaan0401 <99709495+Lakshaan0401@users.noreply.github.com>
2023-07-11 19:01:32 +0530

    Update signup.js

--- signup.js
+++ signup.js
@@ -1,16 +1,18 @@
-function signup(){
+function signup() {
     user_name = document.getElementById("user_name").value;
     password = document.getElementById("password").value;
     email = document.getElementById("email").value;
-    localStorage.setItem("email",email);
-    localStorage.setItem("user_name",user_name);
-    localStorage.setItem("password",password);
-    let showPasswordBtn = document.querySelector('.show-password');
+    localStorage.setItem("email", email);
+    localStorage.setItem("user_name", user_name);
+    localStorage.setItem("password", password);
+    window.location = "dishat_room.html";
+}
+
+let showPasswordBtn = document.querySelector('.show-password');
 let passwordInp = document.querySelector('.password-input');
 
 showPasswordBtn.addEventListener('click', () => {
-
-    showPasswordBtn.classList.toggle('fa-eye'); 
+    showPasswordBtn.classList.toggle('fa-eye');
     showPasswordBtn.classList.toggle('fa-eye-slash');
     passwordInp.type = passwordInp.type === 'password' ? 'text' : 'password';
 
@@ -22,8 +24,8 @@ let validationRegex = [
     { regex: /.{8,}/ },
     { regex: /[0-9]/ },
     { regex: /[a-z]/ },
-    { regex: /[A-Z]/},
-    { regex: /[^A-Za-z0-9]/}
+    { regex: /[A-Z]/ },
+    { regex: /[^A-Za-z0-9]/ }
 ]
 
 passwordInp.addEventListener('keyup', () => {
@@ -31,18 +33,16 @@ passwordInp.addEventListener('keyup', () => {
 
         let isValid = item.regex.test(passwordInp.value);
 
-        if(isValid) {
+        if (isValid) {
             passwordChecklist[i].classList.add('checked');
-        } else{
+        } else {
             passwordChecklist[i].classList.remove('checked');
         }
 
     })
 })
-    window.location = "dishat_room.html";
-}
 
-function login(){
+function login() {
     window.location = "index.html";
 }
 

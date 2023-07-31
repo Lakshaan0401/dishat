59e9c510722e4b0af08503c4b07701600341d9b2
Lakshaan0401 <99709495+Lakshaan0401@users.noreply.github.com>
2023-07-31 19:01:09 +0530

    Update signup.js

--- signup.js
+++ signup.js
@@ -1,48 +1,63 @@
+var score = "";
 function signup() {
     user_name = document.getElementById("user_name").value;
     password = document.getElementById("password").value;
     email = document.getElementById("email").value;
-    localStorage.setItem("email", email);
-    localStorage.setItem("user_name", user_name);
-    localStorage.setItem("password", password);
-    window.location = "dishat_room.html";
+    if (email == "") {
+        window.alert("Please Enter Your Email ID")
+    }
+    else if (user_name == "") {
+        window.alert("Please Enter Your User Name")
+    }
+    else if (password == "") {
+        window.alert("Please Enter Your Password")
+    }
+    else {
+        localStorage.setItem("email", email);
+        localStorage.setItem("user_name", user_name);
+        window.location = "dishat_room.html";
+    }
+    if(score == "set"){
+        localStorage.setItem("password", password);
+        window.location = "dishat_room.html"
+    }
 }
 
 let showPasswordBtn = document.querySelector('.show-password');
-let passwordInp = document.querySelector('.password-input');
+        let passwordInp = document.querySelector('.password-input');
 
-showPasswordBtn.addEventListener('click', () => {
-    showPasswordBtn.classList.toggle('fa-eye');
-    showPasswordBtn.classList.toggle('fa-eye-slash');
-    passwordInp.type = passwordInp.type === 'password' ? 'text' : 'password';
+        showPasswordBtn.addEventListener('click', () => {
+            showPasswordBtn.classList.toggle('fa-eye');
+            showPasswordBtn.classList.toggle('fa-eye-slash');
+            passwordInp.type = passwordInp.type === 'password' ? 'text' : 'password';
 
-})
+        })
 
-let passwordChecklist = document.querySelectorAll('.list-item');
+        let passwordChecklist = document.querySelectorAll('.list-item');
 
-let validationRegex = [
-    { regex: /.{8,}/ },
-    { regex: /[0-9]/ },
-    { regex: /[a-z]/ },
-    { regex: /[A-Z]/ },
-    { regex: /[^A-Za-z0-9]/ }
-]
+        let validationRegex = [
+            { regex: /.{8,}/ },
+            { regex: /[0-9]/ },
+            { regex: /[a-z]/ },
+            { regex: /[A-Z]/ },
+            { regex: /[^A-Za-z0-9]/ }
+        ]
 
-passwordInp.addEventListener('keyup', () => {
-    validationRegex.forEach((item, i) => {
+        passwordInp.addEventListener('keyup', () => {
+            validationRegex.forEach((item, i) => {
 
-        let isValid = item.regex.test(passwordInp.value);
+                let isValid = item.regex.test(passwordInp.value);
 
-        if (isValid) {
-            passwordChecklist[i].classList.add('checked');
-        } else {
-            passwordChecklist[i].classList.remove('checked');
-        }
+                if (isValid) {
+                    passwordChecklist[i].classList.add('checked');
+                    score = "set"
+                } else {
+                    passwordChecklist[i].classList.remove('checked');
+                }
 
-    })
-})
+            })
+        })
 
 function login() {
     window.location = "index.html";
 }
-
